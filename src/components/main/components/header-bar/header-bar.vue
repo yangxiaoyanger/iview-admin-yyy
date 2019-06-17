0f414bcd2a9afe86d7b87635302cd56583e29784
ytt <737815616@qq.com>
2019-06-17 16:41:40 +0800

    hhh

---
+++ src/components/main/components/header-bar/header-bar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="header-bar">
+    <nav-menu :list="navMenuList"></nav-menu>
+    <!-- 需要放在菜单上面的内容，如Logo，写在side-menu标签内部，如下 -->
+        <!-- <div class="logo-con">
+          <img v-show="!collapsed" :src="maxLogo" key="max-logo" />
+          <img v-show="collapsed" :src="minLogo" key="min-logo" />
+        </div> -->
+    <!-- <custom-bread-crumb show-icon style="margin-left: 30px;" :list="breadCrumbList"></custom-bread-crumb> -->
+    <div class="custom-content-con">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+import NavMenu from './nav-menu'
+import './header-bar.less'
+import { constants } from 'crypto';
+export default {
+  name: 'HeaderBar',
+  components: {
+    NavMenu
+  },
+  props: {
+    
+  },
+  computed: {
+    navMenuList() {
+      console.log(this.$store.getters.navMenuList)
+      return this.$store.getters.navMenuList
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
---
+++ src/components/main/components/header-bar/nav-menu/nav-menu.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="nav-menu-bar">
+      <Menu mode="horizontal">
+          <div class="layout-logo"></div>
+          <div class="layout-nav">
+            <template  v-for="item in list">
+              <MenuItem :key="`menu-${item.name}`" >
+                  <Icon type="ios-navigate"></Icon>
+                  {{item.name}}
+              </MenuItem>
+            </template>
+          </div>
+      </Menu>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'NavMenu',
+  components: {
+  },
+  props: {
+    list: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  methods: {
+  },
+  computed: {
+  },
+  watch: {
+    
+  },
+  mounted () {
+    
+  }
+}
+</script>
+<style lang="less">
+@import './nav-menu.less';
+</style>
