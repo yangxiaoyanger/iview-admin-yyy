752c860701476acf48e29b2214d2e8c9788193ed
ytt <737815616@qq.com>
2019-06-25 17:23:10 +0800

    table

--- src/view/components/demotable/demotable.vue
+++ src/view/components/demotable/demotable.vue
@@ -20,60 +20,126 @@
     :height="fixedHeader ? 250 : ''" :size="tableSize" :data="tableData3" :columns="tableColumns3"></Table>
      <Page :total="100" show-sizer show-elevator show-total :styles="pageStyle"/>
 
-     <Modal v-model="modal12" draggable scrollable title="编辑弹窗" width="70">
-        <Form :model="formItem" :label-width="80">
-        <FormItem label="Input">
-            <Input v-model="formItem.input" placeholder="Enter something..."></Input>
-        </FormItem>
-        <FormItem label="Select">
-            <Select v-model="formItem.select">
-                <Option value="beijing">New York</Option>
-                <Option value="shanghai">London</Option>
-                <Option value="shenzhen">Sydney</Option>
-            </Select>
-        </FormItem>
-        <FormItem label="DatePicker">
-            <Row>
-                <Col span="4">
-                    <DatePicker type="date" placeholder="Select date" v-model="formItem.date"></DatePicker>
-                </Col>
-                <Col span="1" style="text-align: center">-</Col>
-                <Col span="4">
-                    <TimePicker type="time" placeholder="Select time" v-model="formItem.time"></TimePicker>
-                </Col>
-            </Row>
-        </FormItem>
-        <FormItem label="Radio">
-            <RadioGroup v-model="formItem.radio">
-                <Radio label="male">Male</Radio>
-                <Radio label="female">Female</Radio>
-            </RadioGroup>
-        </FormItem>
-        <FormItem label="Checkbox">
-            <CheckboxGroup v-model="formItem.checkbox">
-                <Checkbox label="Eat"></Checkbox>
-                <Checkbox label="Sleep"></Checkbox>
-                <Checkbox label="Run"></Checkbox>
-                <Checkbox label="Movie"></Checkbox>
-            </CheckboxGroup>
-        </FormItem>
-        <FormItem label="Switch">
-            <i-switch v-model="formItem.switch" size="large">
-                <span slot="open">On</span>
-                <span slot="close">Off</span>
-            </i-switch>
-        </FormItem>
-        <FormItem label="Slider">
-            <Slider v-model="formItem.slider" range></Slider>
-        </FormItem>
-        <FormItem label="Text">
-            <Input v-model="formItem.textarea" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="Enter something..."></Input>
-        </FormItem>
-        <!-- <FormItem>
-            <Button type="primary">Submit</Button>
-            <Button style="margin-left: 8px">Cancel</Button>
-        </FormItem> -->
+    <Modal v-model="editModel" draggable scrollable width="70">
+       <p slot="header">
+            <span>编辑弹窗</span>
+        </p>
+        <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="80" inline>
+          <FormItem label="Name" prop="name">
+              <Input v-model="formValidate.name" placeholder="Enter your name"></Input>
+          </FormItem>
+          <FormItem label="E-mail" prop="mail">
+              <Input v-model="formValidate.mail" placeholder="Enter your e-mail"></Input>
+          </FormItem>
+          <FormItem label="City" prop="city">
+              <Select v-model="formValidate.city" placeholder="Select your city">
+                  <Option value="beijing">New York</Option>
+                  <Option value="shanghai">London</Option>
+                  <Option value="shenzhen">Sydney</Option>
+              </Select>
+          </FormItem>
+          <FormItem label="Date">
+              <Row>
+                  <Col span="11">
+                      <FormItem prop="date">
+                          <DatePicker type="date" placeholder="Select date" v-model="formValidate.date"></DatePicker>
+                      </FormItem>
+                  </Col>
+                  <Col span="2" style="text-align: center">-</Col>
+                  <Col span="11">
+                      <FormItem prop="time">
+                          <TimePicker type="time" placeholder="Select time" v-model="formValidate.time"></TimePicker>
+                      </FormItem>
+                  </Col>
+              </Row>
+          </FormItem>
+          <FormItem label="Gender" prop="gender">
+              <RadioGroup v-model="formValidate.gender">
+                  <Radio label="male">Male</Radio>
+                  <Radio label="female">Female</Radio>
+              </RadioGroup>
+          </FormItem>
+          <FormItem label="Hobby" prop="interest">
+              <CheckboxGroup v-model="formValidate.interest">
+                  <Checkbox label="Eat"></Checkbox>
+                  <Checkbox label="Sleep"></Checkbox>
+                  <Checkbox label="Run"></Checkbox>
+                  <Checkbox label="Movie"></Checkbox>
+              </CheckboxGroup>
+          </FormItem>
+          <FormItem label="Desc" prop="desc">
+              <Input v-model="formValidate.desc" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="Enter something..."></Input>
+          </FormItem>
+          <!-- <FormItem>
+              <Button type="primary" @click="handleSubmit('formValidate')">Submit</Button>
+              <Button @click="handleReset('formValidate')" style="margin-left: 8px">Reset</Button>
+          </FormItem> -->
       </Form>
+      <div slot="footer">
+          <Button type="primary" @click="handleSubmit('formValidate')">确定</Button>
+          <Button @click="handleReset('formValidate')" style="margin-left: 8px">重置</Button>
+      </div>
+    </Modal>
+
+    <Modal v-model="detailModel" draggable scrollable width="70">
+        <p slot="header">
+            <span>详情弹窗</span>
+        </p>
+        <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="80" inline>
+            <FormItem label="Name" prop="name">
+                <Input v-model="formValidate.name" placeholder="Enter your name"></Input>
+            </FormItem>
+            <FormItem label="E-mail" prop="mail">
+                <Input v-model="formValidate.mail" placeholder="Enter your e-mail"></Input>
+            </FormItem>
+            <FormItem label="City" prop="city">
+                <Select v-model="formValidate.city" placeholder="Select your city">
+                    <Option value="beijing">New York</Option>
+                    <Option value="shanghai">London</Option>
+                    <Option value="shenzhen">Sydney</Option>
+                </Select>
+            </FormItem>
+            <FormItem label="Date">
+                <Row>
+                    <Col span="11">
+                        <FormItem prop="date">
+                            <DatePicker type="date" placeholder="Select date" v-model="formValidate.date"></DatePicker>
+                        </FormItem>
+                    </Col>
+                    <Col span="2" style="text-align: center">-</Col>
+                    <Col span="11">
+                        <FormItem prop="time">
+                            <TimePicker type="time" placeholder="Select time" v-model="formValidate.time"></TimePicker>
+                        </FormItem>
+                    </Col>
+                </Row>
+            </FormItem>
+            <FormItem label="Gender" prop="gender">
+                <RadioGroup v-model="formValidate.gender">
+                    <Radio label="male">Male</Radio>
+                    <Radio label="female">Female</Radio>
+                </RadioGroup>
+            </FormItem>
+            <FormItem label="Hobby" prop="interest">
+                <CheckboxGroup v-model="formValidate.interest">
+                    <Checkbox label="Eat"></Checkbox>
+                    <Checkbox label="Sleep"></Checkbox>
+                    <Checkbox label="Run"></Checkbox>
+                    <Checkbox label="Movie"></Checkbox>
+                </CheckboxGroup>
+            </FormItem>
+            <FormItem label="Desc" prop="desc">
+                <Input v-model="formValidate.desc" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="Enter something..."></Input>
+            </FormItem>
+          <!-- <FormItem>
+              <Button type="primary" @click="handleSubmit('formValidate')">Submit</Button>
+              <Button @click="handleReset('formValidate')" style="margin-left: 8px">Reset</Button>
+          </FormItem> -->
+      </Form>
+      <!-- <div slot="footer">
+          <Button type="primary" @click="handleSubmit('formValidate')">确定</Button>
+          <Button @click="handleReset('formValidate')" style="margin-left: 8px">重置</Button>
+      </div> -->
     </Modal>
   </div>
 </template>
@@ -141,17 +207,46 @@
                 pageStyle: {
                   marginTop: '20px'
                 },
-                modal12: false,
-                formItem: {
-                    input: '',
-                    select: '',
-                    radio: 'male',
-                    checkbox: [],
-                    switch: true,
+                detailModel: false,
+                editModel: false,
+                formValidate: {
+                    name: '',
+                    mail: '',
+                    city: '',
+                    gender: '',
+                    interest: [],
                     date: '',
                     time: '',
-                    slider: [20, 50],
-                    textarea: ''
+                    desc: ''
+                },
+                ruleValidate: {
+                    name: [
+                        { required: true, message: 'The name cannot be empty', trigger: 'blur' }
+                    ],
+                    mail: [
+                        { required: true, message: 'Mailbox cannot be empty', trigger: 'blur' },
+                        { type: 'email', message: 'Incorrect email format', trigger: 'blur' }
+                    ],
+                    city: [
+                        { required: true, message: 'Please select the city', trigger: 'change' }
+                    ],
+                    gender: [
+                        { required: true, message: 'Please select gender', trigger: 'change' }
+                    ],
+                    interest: [
+                        { required: true, type: 'array', min: 1, message: 'Choose at least one hobby', trigger: 'change' },
+                        { type: 'array', max: 2, message: 'Choose two hobbies at best', trigger: 'change' }
+                    ],
+                    date: [
+                        { required: true, type: 'date', message: 'Please select the date', trigger: 'change' }
+                    ],
+                    time: [
+                        { required: true, type: 'string', message: 'Please select time', trigger: 'change' }
+                    ],
+                    desc: [
+                        { required: true, message: 'Please enter a personal introduction', trigger: 'blur' },
+                        { type: 'string', min: 20, message: 'Introduce no less than 20 words', trigger: 'blur' }
+                    ]
                 }
             }
         },
@@ -280,30 +375,39 @@
         },
         methods: {
             show (index) {
-                this.$Modal.info({
-                    title: 'User Info',
-                    content: `Name：${this.tableData3[index].name}<br>Age：${this.tableData3[index].age}<br>Address：${this.tableData3[index].address}`
-                })
+                this.detailModel = true;
             },
             remove (index) {
                 this.tableData3.splice(index, 1);
             },
             edit (index) {
-              this.modal12 = true;
-              // this.$Modal.confirm({
-              //       title: 'Title',
-              //       content: '<p>Content of dialog</p><p>Content of dialog</p>',
-              //       onOk: () => {
-              //           this.$Message.info('Clicked ok');
-              //       },
-              //       onCancel: () => {
-              //           this.$Message.info('Clicked cancel');
-              //       }
-              //   });
+              this.editModel = true;
+            },
+            handleSubmit (name) {
+                this.$refs[name].validate((valid) => {
+                    if (valid) {
+                        this.$Message.success('Success!');
+                    } else {
+                        this.$Message.error('Fail!');
+                    }
+                })
+            },
+            handleReset (name) {
+                this.$refs[name].resetFields();
             }
         }
     }
 </script>
+<style>
+  .ivu-form-inline .ivu-form-item {
+    width: 50%;
+    margin-right: 0;
+  }
+  .ivu-modal-footer {
+    text-align: center
+  }
+</style>
+
 
 
 
